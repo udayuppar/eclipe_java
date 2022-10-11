e21ee3398b213437c03a45561a69641478069610
udayuppar <udayr.xwokz@gmail.com>
2022-10-11 09:26:17 +0530

    hello

---
+++ festival/src/com/uday/voilence/festival/boot/Habba.java
@@ -0,0 +1,14 @@
+package com.uday.voilence.festival.boot;
+
+public class Habba {
+	public Habba()
+	
+	{
+		
+		System.out.println("running");
+	}
+
+	
+}
+ 
+	 
---
+++ festival/src/com/uday/voilence/festival/boot/HabbaRunner.java
@@ -0,0 +1,21 @@
+package com.uday.voilence.festival.boot;
+
+public class HabbaRunner {
+
+	public static void main(String[] args) {
+		
+	new Habba ();
+	new Habba ();
+	new Habba ();
+	new Habba ();
+	new Habba ();
+	System.out.println("end");
+	
+	
+	}
+}
+
+
+
+
+
