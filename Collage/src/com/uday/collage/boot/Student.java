e95e0740577cba910e905f40694b514586bdeb8a
udayuppar <udayr.xwokz@gmail.com>
2022-10-11 09:24:02 +0530

    hello

---
+++ Collage/src/com/uday/collage/boot/Student.java
@@ -0,0 +1,8 @@
+package com.uday.collage.boot;
+
+public class Student {
+ public Student()
+ {
+	 System.out.println("*****");
+ }
+}
---
+++ Collage/src/com/uday/collage/boot/StudentRunner.java
@@ -0,0 +1,13 @@
+
+package com.uday.collage.boot;
+import com.uday.collage.boot.Student;
+public class StudentRunner {
+	public static void main(String[] args)
+	{
+		new Student();
+		new Student();
+		new Student();
+		new Student();
+		new Student();
+	}
+}
