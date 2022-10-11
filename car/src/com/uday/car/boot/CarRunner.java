1a287375e58d8ec2b0102df12573b5206472dc22
udayuppar <udayr.xwokz@gmail.com>
2022-10-11 09:21:06 +0530

    hello

---
+++ car/src/com/uday/car/boot/CarRunner.java
@@ -0,0 +1,13 @@
+package com.uday.car.boot;
+import com.uday.car.boot.Car;
+public class CarRunner {
+
+	public static void main(String[] args) {
+		new Car();
+		new Car();
+		new Car();
+		new Car();
+		new Car();
+	}
+
+}
