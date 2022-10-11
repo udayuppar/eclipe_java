358e88048380300bbb9eebc4b4f1118d10b7fcbc
udayuppar <udayr.xwokz@gmail.com>
2022-10-11 09:31:43 +0530

    hello

---
+++ laptop/src/com/uday/laptop/boot/Laptop.java
@@ -0,0 +1,5 @@
+package com.uday.laptop.boot;
+
+public class Laptop {
+
+}
---
+++ laptop/src/com/uday/laptop/boot/LaptopRunner.java
@@ -0,0 +1,16 @@
+package com.uday.laptop.boot;
+
+public class LaptopRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Laptop();
+		new Laptop();
+		new Laptop();
+		new Laptop();
+		new Laptop();
+		
+
+	}
+
+}
