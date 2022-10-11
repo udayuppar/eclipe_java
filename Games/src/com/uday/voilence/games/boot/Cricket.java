57783dada9ec6da87e3f13c825a60778668004dc
udayuppar <udayr.xwokz@gmail.com>
2022-10-11 09:28:17 +0530

    hello

---
+++ Games/src/com/uday/voilence/games/boot/Cricket.java
@@ -0,0 +1,8 @@
+package com.uday.voilence.games.boot;
+
+public class Cricket {
+	public Cricket()
+	{
+		System.out.println("starting");
+}
+}
---
+++ Games/src/com/uday/voilence/games/boot/CricketRunner.java
@@ -0,0 +1,14 @@
+package com.uday.voilence.games.boot;
+
+public class CricketRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Cricket();
+		new Cricket();
+		new Cricket();
+		new Cricket();
+		new Cricket();
+	}
+
+}
