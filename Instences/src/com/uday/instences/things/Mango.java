b41d5c695fe45ac5101882eac1b63154763879b4
udayuppar <udayr.xwokz@gmail.com>
2022-10-26 09:36:29 +0530

    instance

--- Instences/src/com/uday/instences/things/Mango.java
+++ Instences/src/com/uday/instences/things/Mango.java
@@ -1,6 +1,8 @@
 
 package com.uday.instences.things;
 
+import com.uday.instences.Constructer.MangoType;
+
 public class Mango {
 
 	public final String name = "Totapuri";
@@ -8,6 +10,7 @@ public class Mango {
 	public int quantity;
 	public boolean sweet;
 	public String uses = "pickels";
+	public MangoType THOTAPURI=MangoType.KESAR;
 
 	static {
 		Mango.price = 500;
@@ -27,6 +30,7 @@ public class Mango {
 		System.out.println(this.quantity);
 		System.out.println(this.sweet);
 		System.out.println(this.uses);
+		System.out.println(this.THOTAPURI);
 
 	}
 
