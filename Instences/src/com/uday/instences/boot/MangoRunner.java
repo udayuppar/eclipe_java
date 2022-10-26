7e4ea46dc00e5b6d721d8ae6974aa679f9ea2032
udayuppar <udayr.xwokz@gmail.com>
2022-10-26 09:37:01 +0530

    instance

--- Instences/src/com/uday/instences/boot/MangoRunner.java
+++ Instences/src/com/uday/instences/boot/MangoRunner.java
@@ -9,7 +9,7 @@ public class MangoRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 		Mango mango = new Mango(20);
 		mango.setMango();
 		mango.display();
