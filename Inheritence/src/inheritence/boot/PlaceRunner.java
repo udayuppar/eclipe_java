3cb19ef545c4bb497fd923a832fe39d36c643156
udayuppar <udayr.xwokz@gmail.com>
2022-11-06 22:27:10 +0530

    Inheritence

---
+++ Inheritence/src/inheritence/boot/PlaceRunner.java
@@ -0,0 +1,46 @@
+package com.uday.inheritence.boot;
+
+import com.uday.inheritence.things.*;
+import com.uday.inheritence.things.Object;
+
+public class PlaceRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Place place = new Place();
+		Object place2 = new Place();
+
+		Object capital = new Capital();
+		Capital capital2 = new Capital();
+		Place capial3 = new Capital();
+
+		City city = new City();
+		Capital capital4 = new City();
+		Object obj = new City();
+		Place place3 = new City();
+
+		MetroCity metro = new MetroCity();
+		City metro2 = new MetroCity();
+		Capital metro3 = new MetroCity();
+		Place metro4 = new MetroCity();
+		Object metro5 = new MetroCity();
+
+		SiliconCity silicon = new SiliconCity();
+		MetroCity silicon2 = new SiliconCity();
+		City silicon3 = new SiliconCity();
+		Capital silicon4 = new SiliconCity();
+		Place silicon5 = new SiliconCity();
+		Object silicon6 = new SiliconCity();
+
+		Bengalur beng = new Bengalur();
+		SiliconCity beng2 = new Bengalur();
+		MetroCity beng3 = new Bengalur();
+		City beng4 = new Bengalur();
+		Capital beng5 = new Bengalur();
+		Place beng6 = new Bengalur();
+		Object beng7 = new Bengalur();
+
+	}
+
+}
