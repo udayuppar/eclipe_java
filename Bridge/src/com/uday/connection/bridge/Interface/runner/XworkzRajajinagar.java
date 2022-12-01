2daf60ea14f91be7e23632c5c0534b660cdab35d
udayuppar <udayr.xwokz@gmail.com>
2022-12-01 23:21:58 +0530

    interface using multiple inheritence

---
+++ Bridge/src/com/uday/connection/bridge/Interface/runner/XworkzRajajinagar.java
@@ -0,0 +1,54 @@
+package com.uday.connection.bridge.Interface.runner;
+
+import com.uday.connection.bridge.Interface.follower.Xworkz;
+import com.uday.connection.bridge.Interface.rulls.DevolopmentInstritute;
+import com.uday.connection.bridge.Interface.rulls.Instritute;
+import com.uday.connection.bridge.Interface.rulls.TestingInstritute;
+import com.uday.connection.bridge.Interface.rulls.TrafficRulls;
+
+public class XworkzRajajinagar {
+
+	public static void main(String[] args) {
+		Object obj=new Xworkz();
+		System.out.println(System.lineSeparator());
+		
+		Xworkz xworkz=new Xworkz();
+		xworkz.interview();
+		xworkz.placement();
+		xworkz.training();
+		System.out.println(System.lineSeparator());
+		
+		Instritute instritute =new Xworkz();
+		instritute.placement();
+		instritute.training();
+		System.out.println(System.lineSeparator());
+		
+		DevolopmentInstritute devolopment=new Xworkz();
+		devolopment.interview();
+		devolopment.placement();
+		devolopment.training();
+		System.out.println(System.lineSeparator());
+		
+		TestingInstritute test=new Xworkz();
+		test.interview();
+		test.placement();
+		test.training();
+		System.out.println(System.lineSeparator());
+		
+		TrafficRulls traffic =new Xworkz();
+		traffic.tripleRidding();
+		traffic.wearHelmet();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
