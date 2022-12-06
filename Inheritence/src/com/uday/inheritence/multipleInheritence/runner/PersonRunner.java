a60dfad6f4547a5dedb3904b8abc6b4cbfb153b8
udayuppar <udayr.xwokz@gmail.com>
2022-12-06 22:00:36 +0530

    multiple inheritence

---
+++ Inheritence/src/com/uday/inheritence/multipleInheritence/runner/PersonRunner.java
@@ -0,0 +1,32 @@
+package com.uday.inheritence.multipleInheritence.runner;
+
+import com.uday.inheritence.multipleInheritence.word.VijayaLakshmi;
+
+public class PersonRunner {
+
+	public static void main(String[] args) {
+		VijayaLakshmi vijayaLakshmi=new VijayaLakshmi();
+		vijayaLakshmi.completedProgram();
+		vijayaLakshmi.eat();
+		vijayaLakshmi.executeProgram();
+		vijayaLakshmi.getLocation();
+		vijayaLakshmi.getMobileNum();
+		vijayaLakshmi.getName();
+		vijayaLakshmi.getSalary();
+		vijayaLakshmi.gst();
+		vijayaLakshmi.isGoodPerson();
+		vijayaLakshmi.paidTax();
+		vijayaLakshmi.setGoodPerson(true);
+		vijayaLakshmi.setLocation("Tarikere");
+		vijayaLakshmi.setMobileNum(8861539422l);
+		vijayaLakshmi.setName("Uday");
+		vijayaLakshmi.setSalary(50000);
+		vijayaLakshmi.sleep();
+		vijayaLakshmi.training();
+		vijayaLakshmi.uploadedTask();
+		vijayaLakshmi.vat();
+		
+
+	}
+
+}
