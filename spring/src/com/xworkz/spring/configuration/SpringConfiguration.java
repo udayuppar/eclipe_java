ba77db1bd5e12485a222717f1d6104eb46479a61
udayuppar <udayr.xwokz@gmail.com>
2023-01-06 21:50:46 +0530

    Spring basic

---
+++ spring/src/com/xworkz/spring/configuration/SpringConfiguration.java
@@ -0,0 +1,12 @@
+package com.xworkz.spring.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration //is a annotation mark the class has to configuration
+@ComponentScan("com.xworkz.spring.things")//is to scan the package is to create an instance
+
+
+public class SpringConfiguration {
+
+}
